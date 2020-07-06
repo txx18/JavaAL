ebe93c3ea757498e58075c438f935e8a42a3e41f
Shane Tang <912841857@qq.com>
2020-07-06 11:08:31 +0800

    20200704

---
+++ src/main/java/tree/BST.java
@@ -0,0 +1,16 @@
+package tree;
+
+
+/**
+ * 二叉树查找（基于二叉查找树）
+ * @author Shane Tang
+ * @create 2020-06-14 23:00
+ */
+public class BST<T extends Comparable<? super T>> {
+
+    public BSTNode root;
+
+
+    private class BSTNode {
+    }
+}
